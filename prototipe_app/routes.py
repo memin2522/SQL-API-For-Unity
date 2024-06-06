b9ffec49705fae353b96f7ea22390778f8c7810d
Memo Reyes <sergio@yeltic.com>
2024-06-05 21:02:19 -0500

    Added add patient route

--- prototipe_app/routes.py
+++ prototipe_app/routes.py
@@ -1,7 +1,8 @@
 
 from flask import (
     Blueprint,
-    jsonify
+    jsonify, 
+    request
 )
 
 from .db import get_db
@@ -22,3 +23,47 @@ def get_all_patients():
     cursor.close()
     return jsonify(patients)
 
+@main.post("/add-patient")
+def add_patient():
+    data = request.get_json()
+    required_fields = ['registration', 'firstName', 'lastName', 'dateOfBirth', 'militaryStatus', 'militaryRank']
+
+    for field in required_fields:
+        if field not in data:
+            return jsonify({
+                "error": f"El campo {field} es requerido"
+            }), 400
+
+    registration = data['registration']
+    firstName = data['firstName']
+    lastName = data['lastName']
+    dateOfBirth = data['dateOfBirth']
+    militaryStatus = data['militaryStatus']
+    militaryRank = data['militaryRank']
+
+    db = get_db()
+    cursor = db.cursor()
+
+    try:
+        cursor.execute("""
+            INSERT INTO patient (registration, firstName, lastName, dateOfBirth, militaryStatus, militaryRank)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """, (registration, firstName, lastName, dateOfBirth, militaryStatus, militaryRank))
+        db.commit()
+        current_id = cursor.lastrowid
+        cursor.close()
+        return jsonify({
+            "id": current_id,
+            "registration": registration,
+            "firstName": firstName,
+            "lastName": lastName,
+            "dateOfBirth": dateOfBirth,
+            "militaryStatus": militaryStatus,
+            "militaryRank": militaryRank
+        }), 201
+    except Exception as e:
+        db.rollback()
+        cursor.close()
+        return jsonify({
+            "error": str(e)
+        }), 500
