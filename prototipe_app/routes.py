8e5a16853822668c437aeedb4592336002d8905f
Memo Reyes <sergio@yeltic.com>
2024-06-05 19:53:47 -0500

    Added new index route to the API

--- prototipe_app/routes.py
+++ prototipe_app/routes.py
@@ -10,6 +10,11 @@ main = Blueprint("main", __name__)
 
 @main.get("/")
 def index():
+    return "<h1>Esta es la pagina principal de la API de yeltic para el protipo de Sedena</h1>"
+
+
+@main.get("/all-patients")
+def get_all_patients():
     db = get_db()
     cursor = db.cursor(dictionary=True)
     cursor.execute("SELECT * FROM patient;")
