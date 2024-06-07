b512dce580c043815ee40d43e43e758c52d575b4
Memo Reyes <serrey04005@gmail.com>
2024-06-07 10:34:52 -0500

    Grant_acces to whitelist fixes and testing

--- ip_whitelist/grant_access.py
+++ ip_whitelist/grant_access.py
@@ -4,17 +4,20 @@ whitelist_dict = {}
 
 def fill_whitelist_dict():
     with open('./ip_whitelist/whitelist.csv', mode='r') as whitelist:
-        whitelist_dict = csv.DictReader(whitelist)
-        for row in whitelist_dict:
-            print(row)
+        reader = csv.DictReader(whitelist)
+        for row in reader:
+            ip = row.get("ip")
+            port = row.get("port")
+            whitelist_dict[ip] = port
     if whitelist_dict:
         grant_access_to_whitelist()
     else:
         print("There is no ip's to register")
 
 def grant_access_to_whitelist():
+    os.system("sudo ufw reset")
+    print(whitelist_dict)
     for ip, port in whitelist_dict.items():
-        command = f"sudo ufw allow from {ip} to any port {port}"
-        os.system(command)
-    command = "sudo ufw enable"
-    os.system(command)
+        os.system(f"sudo ufw allow from {ip} to any port {port}")
+    os.system("sudo ufw enable")    
+    os.system("sudo ufw status")
