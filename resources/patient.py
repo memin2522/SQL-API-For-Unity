f63f6c2def94e62d588af3abbf1321461cb59e45
Memo Reyes <sergio@yeltic.com>
2024-06-11 17:17:29 -0500

    Routes divided by table

--- resources/patient.py
+++ resources/patient.py
@@ -39,7 +39,7 @@ def delete_patient(patient_id):
             cursor.close()
 
 @blp.put("/patient/<string:patient_id>")       
-def put_patient(patient_id):
+def update_patient(patient_id):
     db = get_db()
     cursor = db.cursor(dictionary=True)
     try:
@@ -61,7 +61,6 @@ def put_patient(patient_id):
 
         query_set_part = ", ".join(fields_and_values)
         querySQL = f"UPDATE patient SET {query_set_part} WHERE id = %s;"
-        ##return querySQL
         cursor.execute(querySQL, values)
         db.commit()
 
@@ -75,3 +74,57 @@ def put_patient(patient_id):
         return jsonify({"error": str(e)}), 500
     finally:
         cursor.close()
+
+@blp.get("/all-patients")
+def get_all_patients():
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM patient;")
+    patients = cursor.fetchall()
+    cursor.close()
+    return jsonify(patients)
+
+@blp.post("/add-patient")
+def add_patient():
+    data = request.get_json()
+    required_fields = ['registration', 'firstName', 'lastName', 'dateOfBirth', 'militaryStatus', 'militaryRank']
+
+    for field in required_fields:
+        if field not in data:
+            return jsonify({
+                "error": f"El campo {field} es requerido"
+            }), 400
+
+    registration = data['registration']
+    firstName = data['firstName']
+    lastName = data['lastName']
+    dateOfBirth = data['dateOfBirth']
+    militaryStatus = data['militaryStatus']
+    militaryRank = data['militaryRank']
+
+    db = get_db()
+    cursor = db.cursor()
+
+    try:
+        cursor.execute("""
+            INSERT INTO patient (registration, firstName, lastName, dateOfBirth, militaryStatus, militaryRank)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """, (registration, firstName, lastName, dateOfBirth, militaryStatus, militaryRank))
+        db.commit()
+        current_id = cursor.lastrowid
+        cursor.close()
+        return jsonify({
+            "id": current_id,
+            "registration": registration,
+            "firstName": firstName,
+            "lastName": lastName,
+            "dateOfBirth": dateOfBirth,
+            "militaryStatus": militaryStatus,
+            "militaryRank": militaryRank
+        }), 201
+    except Exception as e:
+        db.rollback()
+        cursor.close()
+        return jsonify({
+            "error": str(e)
+        }), 500
