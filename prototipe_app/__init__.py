aeca6e3a2ed6e0b0ddc9b8eb04ed791526d3a840
Memo Reyes <sergio@yeltic.com>
2024-06-11 11:21:56 -0500

    Refactor and added patient functions

--- prototipe_app/__init__.py
+++ prototipe_app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask 
+from flask_smorest import Api
 from ip_whitelist import grant_access
 from . import db
 from .routes import main
+
+from resources.patient import blp as PatientBlueprint
 
 def create_app():
     app = Flask(__name__)
@@ -12,4 +15,6 @@ def create_app():
     db.init_app(app)
     app.register_blueprint(main)
 
+    api = Api(app)
+    api.register_blueprint(PatientBlueprint)
     return app
---
+++ prototipe_app/resources/patient.py
@@ -0,0 +1,76 @@
+from flask import (
+    jsonify, 
+    request
+)
+from flask_smorest import Blueprint, abort
+from flask.views import MethodView
+from schemas import PatienSchema, PatienUpdateSchema
+from ..db import get_db
+
+
+blp = Blueprint("Patients", __name__, description="Operations on patients")
+
+@blp.route("/patient/<string:patient_id>")
+class Patient(MethodView):
+    def get(self, patient_id):
+        db = get_db()
+        cursor = db.cursor(dictionary=True)
+        try:
+            cursor.execute("SELECT * FROM patient WHERE id = %s;", patient_id)
+            patient = cursor.fetchone()
+            if patient is None:
+                return jsonify({"error": "Patient not found"}), 404
+            return jsonify(patient)
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+        finally:
+            cursor.close()
+    
+    def delete(self, patient_id):
+        db = get_db()
+        cursor = db.cursor(dictionary=True)
+        try:
+            cursor.execute(f"DELETE FROM patient WHERE id = %s;", patient_id)
+            db.commit()
+            return jsonify({"message":"Item Deleted"}), 200
+        except Exception as e:
+            db.rollback()
+            return jsonify({"error": str(e)}), 500
+        finally:
+            cursor.close()
+    
+    def put(self, patient_id):
+        db = get_db()
+        cursor = db.cursor(dictionary=True)
+        try:
+            data = request.get_json()
+            allowed_fields = ['registration', 'firstName', 'lastName', 'dateOfBirth', 'militaryStatus', 'militaryRank']
+
+            fields_query = []
+            values = []
+
+            for field, value in data.items():
+                if field in allowed_fields:
+                    fields_query.append(f"{field} = %s")
+                    values.append(value)
+            
+            if not fields_query:
+                return jsonify({"error": "No data provided to update"}), 400
+            
+            values.append(patient_id)
+
+            query =f"UPDATE patient SET {', '.join(fields_query)} WHERE id = %s"
+            cursor.execute(query,values) 
+            db.commit()
+
+            if cursor.rowcount == 0:
+                return jsonify({"error": "Patient not found"}), 404
+            
+            return jsonify({"error": "No data provided to update"}), 400
+        
+        except Exception as e:
+            db.rollback()
+            return jsonify({"error": str(e)}), 500
+        finally:
+            cursor.close()
+
